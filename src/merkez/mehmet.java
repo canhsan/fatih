964f3e536085ca457c3d3a23633576b49585c2be
canhsan <1979hasancan@gmail.com>
2023-03-08 13:49:25 +0300

    İkici Vrsiyon

--- src/merkez/mehmet.java
+++ src/merkez/mehmet.java
@@ -3,5 +3,6 @@ package merkez;
 public class mehmet {
     public static void main(String[] args) {
         System.out.println("lütfen  bir isim giriniz");
+        System.out.println("Lütfen soy adınızı giriniz");
     }
 }
